1206a69cb1cf8f53ac84c971746785e9b26ed9b2
Ali Ijaz Sheikh <ofrobots@google.com>
2018-12-11 13:09:22 -0800

    build: ensure nvm is in the path

--- testing/continuous.sh
+++ testing/continuous.sh
@@ -5,7 +5,10 @@ node_versions=${node_versions:-8 10 11}
 cd $(dirname)/..
 
 # Install nvm and needed versions of node
+NVM_DIR="$HOME/.nvm"
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.4/install.sh | bash
+source "$NVM_DIR/nvm.sh"
+
 for version in ${node_versions}; do
   set +ex
   nvm install ${version}
