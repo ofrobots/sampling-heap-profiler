1c673d5222f0fdfbb67a90b65d21e2d04aab3dbe
Ali Ijaz Sheikh <ofrobots@google.com>
2018-12-11 12:48:58 -0800

    build: install node versions using nvm

--- testing/continuous.sh
+++ testing/continuous.sh
@@ -1,7 +1,23 @@
 
-set -x
-cd github/sampling-heap-profiler
+set -ex
+node_versions=${node_versions:-8 10 11}
 
-node -p process.versions
-npm install
-npm run test
+cd $(dirname)/..
+
+# Install nvm and needed versions of node
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.4/install.sh | bash
+for version in ${node_versions}; do
+  set +ex
+  nvm install ${version}
+  set -ex
+
+  npm install -g node-gyp
+
+  echo "============================================================"
+  echo "Running tests with Node ${version}"
+  echo "============================================================"
+  rm -rf node_modules
+  npm install
+  npm run test
+done
+
